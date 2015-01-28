9559f0f504f162a9999116fbd1168c01789d702b
lgrube <lgv9483@saintjoe.edu>
2015-01-28 10:47:22 -0600

    modified works_on two copy into database all data entered in

--- company2.sql
+++ company2.sql
@@ -122,6 +122,9 @@ ALTER TABLE works_on OWNER TO lgrube;
 --
 
 COPY department (dname, dnumber, mgr_ssn, mgr_start_date) FROM stdin;
+Research	5	333444555	1988-05-22
+Administration	4	987654321	1995-01-01
+Headquarters	1	888665555	1981-06-19
 \.
 
 
@@ -130,6 +133,13 @@ COPY department (dname, dnumber, mgr_ssn, mgr_start_date) FROM stdin;
 --
 
 COPY dependent (essn, dependent_name, sex, bdate, relationship) FROM stdin;
+333445555	Alice	F	1986-04-05	Daughter
+333445555	Theodore	M	1983-10-25	Son
+333445555	Joy	F	1958-05-03	Spouse
+987654321	Abner	M	1942-02-28	Spouse
+123456789	Michael	M	1988-01-04	Son
+123456789	Alice	F	1988-12-30	Daughter
+123456789	Elizabeth	F	1967-05-05	Spouse
 \.
 
 
@@ -138,6 +148,11 @@ COPY dependent (essn, dependent_name, sex, bdate, relationship) FROM stdin;
 --
 
 COPY dept_locations (dnumber, dlocation) FROM stdin;
+1	Houston
+4	Stafford
+5	Bellaire
+5	Sugarland
+5	Houston    
 \.
 
 
@@ -146,6 +161,14 @@ COPY dept_locations (dnumber, dlocation) FROM stdin;
 --
 
 COPY employee (fname, minit, lname, ssn, bdate, address, sex, salary, super_ssn, dno) FROM stdin;
+John	B	Smith	123456789	1965-01-09	731Fondren,Houston,TX	M	30000.00	333445555	5
+Franklin	T	Wong	333445555	1955-12-08	638Voss,Houston,TX	M	40000.00	888665555	5
+Alicia	J	Zelaya	999887777	1968-01-19	3321Castle,Spring,TX	F	25000.00	987654321	4
+Jennifer	S	Wallace	987654321	1941-06-20	291Berry,Bellaire,TX	F	43000.00	888665555	4
+Ramesh	K	Narayan	666884444	1962-09-15	975 Fire Oak, Humble, TX	M	38000.00	333445555	5
+Joyce	A	English	453453453	1972-07-31	5631 Rice, Houston, TX	F	25000.00	333445555	5
+Ahmad	V	Jabbar	987987987	1969-03-29	980 Dallas, Houston, TX	M	25000.00	987654321	4
+James	E	Borg	888665555	1937-11-10	450 Stone, Houston, TX	M	55000.00	NULL     	1
 \.
 
 
@@ -154,6 +177,12 @@ COPY employee (fname, minit, lname, ssn, bdate, address, sex, salary, super_ssn,
 --
 
 COPY project (pname, pnumber, plocation, dnum) FROM stdin;
+ProductX	1	Bellaire	5
+ProductY	2	Sugarland	5
+ProductZ	3	Houston	5
+Computerization	10	Stafford	4
+Reorganization	20	Houston	1
+Newbenefits	30	Stafford	4
 \.
 
 
@@ -162,6 +191,22 @@ COPY project (pname, pnumber, plocation, dnum) FROM stdin;
 --
 
 COPY works_on (essn, pno, hours) FROM stdin;
+123456789	1	32.5
+123456789	2	7.5
+666884444	3	40.0
+453453453	1	20.0
+453453453	2	20.0
+333445555	2	10.0
+333445555	3	10.0
+333445555	10	10.0
+333445555	20	10.0
+999887777	30	30.0
+999887777	10	10.0
+987987987	10	35.0
+987987987	30	5.0
+987654321	30	20.0
+987654321	20	15.0
+888665555	20	0.0
 \.
 
 
